d8d8bd0ebd5d28688448c08908dca45774c64523
tarlanaliyev <123490498+tarlanaliyev@users.noreply.github.com>
2023-01-29 23:38:23 +0400

    Lstest updates

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+router.all('/*', (req, res, next) => {
+
+    req.app.locals.layout = 'admin';
+    next();
+
+})
 
 router.get('/', (req,res) => {
     res.render("admin/index");  //burda yazdigimiz butun url-lerin evvleine view engine /admin-i avtomatik elave edir
+})
+
+router.get('/dashboard', (req,res) => {
+    res.render("admin/dashboard");
 })
 
 
--- routes/home/index.js
+++ routes/home/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+router.all('/*', (req, res, next) => {   /// admin/index -de olan kodu bura da yazdim. Cunki tekce admin/index-de olanda
+                                                                                             /// her defesinde /admin eledikde url-de layout-u admin olaraq deyisirdi
+    req.app.locals.layout = 'home';                                                          /// biz yeniden home page qayitmaq istesek alinmirdi. Care olaraq eyni kodu bura
+    next();                                                                                  /// da atmali oldum.
+
+})
+
 
 router.get('/', (req,res) => {
     res.render("home/index");  /// views folderinin altinda /home/index.handlebars file-in axtarir
